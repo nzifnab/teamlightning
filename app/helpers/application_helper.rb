8b65931fd9710df1d86bb8ea2b943a503986d898
Nathan Benes <nathan.benes@gmail.com>
2015-04-19 16:18:25 -0600

    Updated files to .haml, and started creating proper layout file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def current_location?(*locations)
+    locations.any? do |location|
+      if location.is_a?(Regexp)
+        request.path =~ location
+      else
+        controller, action = location.split('#')
+
+        result = (controller_path == controller)
+        if action
+          result &&= (action_name == action)
+        end
+        result
+      end
+    end
+  end
 end
